c581703aedd0d5b1dc35bc989a7414fe065a917a
Greg Smith <m226090@usna.edu>
2022-03-21 14:03:31 -0400

    db change

--- db/migrate/20220209185851_create_products.rb
+++ db/migrate/20220209185851_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
-  def change
-    create_table :products do |t|
-      t.text :name
-      t.string :description
-      t.numeric :price
-      t.integer :stock
-      t.timestamps
-    end
+  
+  create_table :products do |t|
+    t.text :name
+    t.string :description
+    t.numeric :price
+    t.integer :stock
+    t.timestamps
   end
+  
 end
